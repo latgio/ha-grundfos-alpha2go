0ec7a73abfa9f89f1afcafe50bef182dbee90a4b
latgio <96993069+latgio@users.noreply.github.com>
2026-07-07 08:21:53 +0200

    Add integration constants

---
+++ custom_components/grundfos_alpha2go/const.py
@@ -0,0 +1,18 @@
+"""Constants for the Grundfos ALPHA2 GO integration."""
+
+DOMAIN = "grundfos_alpha2go"
+
+CONF_ADDRESS = "address"
+CONF_NAME = "name"
+
+DEFAULT_NAME = "ALPHA2 GO"
+DEFAULT_SCAN_INTERVAL = 30
+
+DEVICE_NAME_PREFIX = "Alpha"
+
+ATTR_FLOW = "flow"
+ATTR_HEAD = "head"
+ATTR_SPEED = "speed"
+ATTR_POWER = "power"
+ATTR_VOLTAGE = "voltage"
+ATTR_CURRENT = "current"
